53500422e4b3af240ed8896e343d2910e8a906c3
YEHong1 <870628496@qq.com>
2018-04-25 22:10:38 +0800

    ListViewDemo

--- app/src/main/java/study/liangyehong/android/review/Activity/MainActivity.java
+++ app/src/main/java/study/liangyehong/android/review/Activity/MainActivity.java
@@ -1,28 +1,49 @@
 package study.liangyehong.android.review.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
+import study.liangyehong.android.review.Adapter.AdapterDemo;
 import study.liangyehong.android.review.R;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView list_test;
+    private TextView title_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        init();
+    }
+
+    private void init() {
+        String[] str = {"ListViewDemo","B","C"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,str);
+        list_test.setAdapter(adapter);
+        list_test.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        Intent intent= new Intent(MainActivity.this,AdapterDemo.class);
+                        startActivity(intent);
+                }
+            }
+        });
     }
 
     private void initView() {
         list_test = (ListView) findViewById(R.id.list_test);
-        String[] str = {"A","B","C"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,str);
-        list_test.setAdapter(adapter);
+        title_bar = (TextView) findViewById(R.id.title_bar);
+/*        title_bar.setText("主界面");*/
     }
 }
--- app/src/main/java/study/liangyehong/android/review/Adapter/AdapterDemo.java
+++ app/src/main/java/study/liangyehong/android/review/Adapter/AdapterDemo.java
@@ -1,15 +1,58 @@
 package study.liangyehong.android.review.Adapter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import study.liangyehong.android.review.R;
 
 public class AdapterDemo extends AppCompatActivity {
 
+    private ImageView img;
+    private TextView name;
+    private TextView say;
+    private ListView listview;
+
+    private String names[] = new String[]{"陈奕迅","张家辉","张学友"};
+    private String says[] = new String[]{"祝你阴天快乐！","是兄弟就来贪玩蓝月！","再玩贪玩蓝月就不认你了！"};
+    private int imgId[] = new int[]{R.drawable.img1,R.drawable.img2,R.drawable.img3};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adapter_demo);
+        initView();
+        initData();
+    }
+
+    private void initData() {
+        List<Map<String,Object>> listitem = new ArrayList<Map<String, Object>>();
+        for (int i = 0; i < names.length; i++){
+            Map<String,Object> showitem = new HashMap<String, Object>();
+            showitem.put("img",imgId[i]);
+            showitem.put("name",names[i]);
+            showitem.put("say",says[i]);
+            listitem.add(showitem);
+        }
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getApplicationContext(),
+                listitem,R.layout.list_item,new String[]{"img","name","say"},
+                new int[]{R.id.img,R.id.name,R.id.say});
+        listview.setAdapter(simpleAdapter);
+    }
+
+    private void initView() {
+        img = (ImageView) findViewById(R.id.img);
+        name = (TextView) findViewById(R.id.name);
+        say = (TextView) findViewById(R.id.say);
+        listview = (ListView) findViewById(R.id.list_test1);
     }
 }
